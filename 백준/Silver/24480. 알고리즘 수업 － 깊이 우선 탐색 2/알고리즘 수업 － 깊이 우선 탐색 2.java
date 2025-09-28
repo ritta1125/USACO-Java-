8a85cc0ffdaeab05c69afa21d3dab44afa825d78
ritta1125 <ho0.si1125@gmail.com>
2025-09-28 14:47:55 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 988 ms, Memory: 93128 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  //https://www.acmicpc.net/problem/24480 
+  static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+  static int[] check;
+  static int count;
+
+  public static void DFS(int R){
+    check[R] = count;
+
+    for(int i = 0; i < graph.get(R).size(); i++){
+      int newN = graph.get(R).get(i); 
+
+      if(check[newN] == 0){
+        count++;
+        DFS(newN);
+      }
+    }
+  }
+
+  
+  public static void main(String[] args) throws IOException {
+    //https://www.acmicpc.net/problem/24479
+
+    BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+    PrintWriter pw = new PrintWriter(System.out);
+    StringTokenizer st = new StringTokenizer(r.readLine());
+
+    int N = Integer.parseInt(st.nextToken());
+    int M = Integer.parseInt(st.nextToken());
+    int R = Integer.parseInt(st.nextToken());
+
+    check = new int[N + 1];
+
+    for(int i = 0; i < N+1; i++){
+      graph.add(new ArrayList<>());
+    }
+
+    for(int i = 0; i < M; i++){
+      st = new StringTokenizer(r.readLine());
+      int from = Integer.parseInt(st.nextToken());
+      int to = Integer.parseInt(st.nextToken());
+
+      graph.get(from).add(to);
+      graph.get(to).add(from);
+    }
+
+    for(int i = 0; i < graph.size(); i++){
+      Collections.sort(graph.get(i),Collections.reverseOrder());
+    }
+
+    count = 1;
+
+    DFS(R);
+
+    for(int i = 1; i < check.length; i++){
+      pw.println(check[i]);
+    }
+    
+    pw.close();
+  }
+}
